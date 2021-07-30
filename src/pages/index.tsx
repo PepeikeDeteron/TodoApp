233153c5c1ea606c169b6e1f31ce95659abb0284
deteikepeperon@gmail.com <sasa_yoshi@docomo.ne.jp>
2021-07-30 14:08:46 +0900

    Add: CSSを追加
    Normalize.cssの追加とglobal.cssの適用

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { NextPage } from 'next';
+import React from 'react';
 import Head from 'next/head';
 
-const Home: NextPage = () => {
+const Home: React.FC = () => {
   return (
     <>
       <Head>
