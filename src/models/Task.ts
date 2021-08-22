de48bcea89e0639d569311a8778b1afe88295842
deteikepeperon@gmail.com <sasa_yoshi@docomo.ne.jp>
2021-08-22 23:10:14 +0900

    Remove: 不要になったファイルを削除
    タスク登録機能の修正前のファイルを削除

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,10 +3,3 @@ export type Tasks = {
   dueDate: Date | null;
   priority: number | number[];
 };
-
-export type Task = {
-  id: number; // id
-  value: string; // 入力内容
-  checked: boolean; // 完了・未完了
-  edit: boolean; // 編集
-};
